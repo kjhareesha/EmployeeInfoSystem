4e0e478b445f554bdd1eb5d69ff4f2d417d67282
kjhareesha <53262067+kjhareesha@users.noreply.github.com>
2025-08-29 23:00:15 +0530

    Add files via upload

---
+++ TestGoProject/internal/model/employee.go
@@ -0,0 +1,9 @@
+package model
+
+type Employee struct {
+	ID        int    `json:"id" db:"id"`
+	FirstName string `json:"first_name" db:"first_name"`
+	LastName  string `json:"last_name" db:"last_name"`
+	Email     string `json:"email" db:"email"`
+	Position  string `json:"position" db:"position"`
+}
